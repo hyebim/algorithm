9716ea485f9795ac93f0c1d6c5395f0a7d8f54bf
hyebim <114204598+hyebim@users.noreply.github.com>
2026-08-27 21:51:59 +0900

    [level 2] Title: 큰 수 만들기, Time: 4899.08 ms, Memory: 390 MB -BaekjoonHub

---
+++ 프로그래머스/2/42883. 큰 수 만들기/큰 수 만들기.java
@@ -0,0 +1,42 @@
+import java.util.*;
+class Solution {
+    public String solution(String number, int k) {
+        String answer = "";
+        int[] num = new int[number.length()];
+        for(int i = 0; i < number.length(); i++) {
+        	num[i] = number.charAt(i)-'0';
+        }
+        
+        int j = 1;
+        Stack<Integer> stack = new Stack<>();
+        stack.push(num[0]);
+        
+        int cnt = 0;
+        while(!stack.isEmpty() && j < num.length) {
+        	if(stack.peek() > num[j]) {
+        		stack.push(num[j]);
+        	} else if(stack.peek() < num[j]) {
+        		while(!stack.isEmpty() && stack.peek() < num[j] && cnt < k) {
+        			stack.pop();
+            		cnt++;
+        		}
+        		stack.push(num[j]);
+        	} else {
+        		stack.push(num[j]);
+        	}
+        	j++;
+        }
+        
+        if(cnt == 0) {
+        	while(cnt < k) {
+        		stack.pop();
+        		cnt++;
+        	}
+        }
+        
+        for(int i = 0; i < stack.size(); i++) {
+        	answer += stack.get(i);
+        }
+        return answer;
+    }
+}
