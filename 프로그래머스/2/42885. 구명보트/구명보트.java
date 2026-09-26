afaaec944ce65ae4e14f3a603f7a7cc555fd1dee
hyebim <114204598+hyebim@users.noreply.github.com>
2026-09-26 21:00:18 +0900

    [level 2] Title: 구명보트, Time: 10.57 ms, Memory: 59 MB -BaekjoonHub

--- 프로그래머스/2/42885. 구명보트/구명보트.java
+++ 프로그래머스/2/42885. 구명보트/구명보트.java
@@ -2,20 +2,28 @@ import java.util.*;
 class Solution {
     public int solution(int[] people, int limit) {
         int answer = 0;
+        
         Arrays.sort(people);
         int left = 0;
-        int right = people.length-1;
+        int right = people.length - 1;
         
-        while(left <= right) {
-        	if(people[left]+people[right] > limit) {
-        		right--;
-        		answer++;
-        	} else {
-        		left++;
-        		right--;
-        		answer++;
-        	}
+        while(left < right) {
+            if(people[left]+people[right] <= limit) {
+                answer++;
+                left++;
+                right--;
+            } else if(people[left]+people[right] > limit) {
+                right--;
+                answer++;
+            } 
+            
+            if(left == right) {
+                answer++;
+                left++;
+                right--;
+            }
         }
+        
         return answer;
     }
 }
