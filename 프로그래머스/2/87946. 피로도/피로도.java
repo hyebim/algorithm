83e1525da6429b2495e0c355a3205ae959b73674
hyebim <114204598+hyebim@users.noreply.github.com>
2026-09-26 21:34:49 +0900

    [level 2] Title: 피로도, Time: 2.11 ms, Memory: 81.5 MB -BaekjoonHub

--- 프로그래머스/2/87946. 피로도/피로도.java
+++ 프로그래머스/2/87946. 피로도/피로도.java
@@ -1,23 +1,25 @@
 class Solution {
-    int answer = 0;
     boolean[] visited;
+    int answer = Integer.MIN_VALUE;
+    
     public int solution(int k, int[][] dungeons) {
-        
+             
         visited = new boolean[dungeons.length];
-        DFS(k, dungeons, 0);
+        DFS(k, dungeons, 0, k);
         
         return answer;
     }
     
-    public void DFS(int k, int[][] dungeons, int count) {
-        answer = Math.max(answer, count);
+    public void DFS(int k, int[][] dungeons, int count, int rest) {
         for(int i = 0; i < dungeons.length; i++) {
-            if(!visited[i] && k >= dungeons[i][0]) {
-                visited[i] = true;
-                DFS(k-dungeons[i][1], dungeons, count+1);
-                visited[i] = false;
-            }
+            answer = Math.max(answer, count);
             
+            if(!visited[i] && dungeons[i][0] <= rest) {
+                visited[i] = true;
+                DFS(k, dungeons, count+1, rest-dungeons[i][1]);
+                visited[i] = false;
+            } 
         }
+    
     }
 }
