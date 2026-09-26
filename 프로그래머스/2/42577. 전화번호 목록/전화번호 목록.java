50ca37c607bb9de77035c4cd3573a01495bc2825
hyebim <114204598+hyebim@users.noreply.github.com>
2026-09-26 22:07:21 +0900

    [level 2] Title: 전화번호 목록, Time: 332.70 ms, Memory: 241 MB -BaekjoonHub

--- 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.java
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.java
@@ -1,18 +1,19 @@
 import java.util.*;
 class Solution {
     public boolean solution(String[] phone_book) {
- 
+        boolean answer = true;
+        
         HashSet<String> set = new HashSet<>();
         for(int i = 0; i < phone_book.length; i++) {
             set.add(phone_book[i]);
         }
         
-        for(int i = 0; i < phone_book.length; i++) {
-            for(int j = 1; j < phone_book[i].length(); j++) {
-                String prefix = phone_book[i].substring(0, j);
-                if(set.contains(prefix)) {
-                    return false;
-                }
+        Arrays.sort(phone_book);
+        
+        for(int i = 0; i < phone_book.length-1; i++) {
+            for(int j = 1; j < phone_book[i+1].length(); j++) {
+                String prefix = phone_book[i+1].substring(0, j);
+                if(prefix.equals(phone_book[i])) return false;
             }
         }
         
