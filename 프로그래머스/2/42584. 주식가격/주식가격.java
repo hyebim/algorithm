d196acbf6e5ebcc0dba8eeceb7d86aa4d0b4e255
hyebim <114204598+hyebim@users.noreply.github.com>
2026-09-26 20:33:48 +0900

    [level 2] Title: 주식가격, Time: 20.65 ms, Memory: 76.8 MB -BaekjoonHub

--- 프로그래머스/2/42584. 주식가격/주식가격.java
+++ 프로그래머스/2/42584. 주식가격/주식가격.java
@@ -5,27 +5,27 @@ class Solution {
         answer = new int[prices.length];
         
         Stack<Integer> stack = new Stack<>();
-        stack.push(0);
-        int idx = 0;
-        for(int i = 1; i < prices.length; i++) {
-//        	if(prices[stack.peek()] < prices[i]) {
-//        		answer[i-1] = 1;
-//        	}
-        	while(!stack.isEmpty() && prices[stack.peek()] > prices[i]) {
-        		idx = stack.pop();
-            	answer[idx] = i - idx;
-        	}
-        	stack.push(i);
-        	
-        }
-        System.out.println(stack);
         
-        
-        for(int i = 0; i < stack.size(); i++) {
-        	int num = stack.get(i);
-        	answer[num] = answer.length-num-1;
-        	
+        for(int i = 0; i < prices.length; i++) {
+            while(!stack.isEmpty() &&
+                    prices[stack.peek()] > prices[i]) {
+                
+                int idx = stack.pop();
+                answer[idx] = i - idx;
+            }
+            
+            stack.push(i);
         }
+        
+        // for(int i = 0; i < prices.length; i++) {
+        //     System.out.println(answer[i]);
+        // }
+
+        while(!stack.isEmpty()) {
+            int idx = stack.pop();
+            answer[idx] = prices.length - idx - 1;
+        }       
+        
         return answer;
     }
 }
